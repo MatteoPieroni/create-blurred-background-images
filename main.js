bd02d7baf9b32eca9ddab87a4d86a86a0d28914c
Matteo Pieroni <matteopieroni6@gmail.com>
2019-05-24 21:00:12 +0100

    reverted to main.js

--- main.js
+++ main.js
@@ -1,2 +1,40 @@
-require("ts-node").register(); // This will register the TypeScript compiler
-require("./src"); // This will load our Typescript application
+const { app, BrowserWindow } = require("electron");
+const deleteTemp = require("./lib/utils");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile("dist/index.html");
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", async () => {
+  try {
+    await deleteTemp();
+  } catch (e) {
+    console.log("There has been a problem in deleting the temp file: ", e);
+  }
+
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
